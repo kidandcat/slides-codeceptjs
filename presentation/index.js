976ef4f444d73ec5b790fce206585bc22928f222
Jairo <jairo.viciana@accenture.com>
2018-05-07 18:06:12 +0200

    up

--- presentation/index.js
+++ presentation/index.js
@@ -2,13 +2,33 @@
 import React from "react";
 
 // Import Spectacle Core tags
-import { Deck, Heading, Slide, Text, CodePane } from "spectacle";
+import { Deck, Heading, Slide, Text, Image } from "spectacle";
 
 // Import theme
 import createTheme from "spectacle/lib/themes/default";
 
 // Require CSS
 require("normalize.css");
+
+const indexCreator = data => ({ active }) => (
+  <div>
+    {data.map((d, i) => (
+      <p key={i} style={{ color: active === i ? "#03A9FC" : "black" }}>
+        {d}
+      </p>
+    ))}
+  </div>
+);
+
+const Index = indexCreator([
+  "¿Qué es un Framework?",
+  "¿Qué ventajas tiene utilizar un Framework?",
+  "¿Y si no necesito o no quiero utilizar un Framework?",
+  "Frontend Frameworks 2018",
+  "React y similares (tipo UI library)",
+  "Angular y similares (tipo full framework)",
+  "Comparaciones en relación al mercado laboral"
+]);
 
 const theme = createTheme(
   {
@@ -23,35 +43,155 @@ const theme = createTheme(
   }
 );
 
+let animationDelay;
+const imgStyle = (delay, first) => {
+  animationDelay = first ? delay : parseInt(delay) + parseInt(animationDelay);
+  return {
+    animationDelay: `${animationDelay}ms`,
+    animationDuration: `${delay * 30}ms`,
+    maxHeight: "100px",
+    flex: 1,
+    margin: "5px",
+    transition: "all .3s linear"
+  };
+};
+
 export default class Presentation extends React.Component {
+  constructor() {
+    super();
+    this.reversed = false;
+    this.state = {
+      logos: [
+        "/dist/angular.png",
+        "/dist/aurelia.png",
+        "/dist/ember.png",
+        "/dist/hyperapp.png",
+        "/dist/inferno.png",
+        "/dist/polymer.png",
+        "/dist/react.png",
+        "/dist/preact.png",
+        "/dist/riot.png",
+        "/dist/vue.png"
+      ]
+    };
+  }
+  reverse() {
+    setTimeout(() => {
+      this.reversed = true;
+      this.forceUpdate();
+    }, 3000);
+  }
   render() {
+    !this.reversed && this.reverse();
     return (
       <Deck transition={["zoom"]} transitionDuration={500} theme={theme}>
         <Slide>
-          <Heading size={1}>React</Heading>
-          <Text>A JavaScript library for building user interfaces</Text>
+          <Heading size={1}>Frontend frameworks</Heading>
         </Slide>
         <Slide>
-          <Heading size={4}>Declarative</Heading>
+          <Index active={0} />
+        </Slide>
+        <Slide>
           <Text>
-            Design simple views for each state in your application, and React
-            will efficiently update and render just the right components when
-            your data changes.
+            Siendo muy simples, es un patrón para el desarrollo de una
+            aplicación
           </Text>
         </Slide>
         <Slide>
-          <CodePane
-            source={`export const Loading = () => (state, actions) => (
-<div style={{ opacity: state.loading ? 1 : 0 }}>
-  <div className="spinner-container">
-    <div className="spinner">
-      <div />
-    </div>
-    <div className="spinner-text">Loading</div>
-  </div>
-</div>
-);`}
-          />
+          <Index active={1} />
+        </Slide>
+        <Slide>
+          <Text>Arquitectura</Text>
+          <p>
+            El programador no necesita plantearse una estructura global de la
+            aplicación, sino que el framework le proporciona un esqueleto
+          </p>
+        </Slide>
+        <Slide>
+          <Text>Facilita la colaboración</Text>
+          <p>
+            Un framework te provee de una estructura y una "forma de hacer las
+            cosas" estandarizada, por lo tanto, cualquiera que conozca cómo
+            funciona ese framework será capaz de entender la mayor parte de la
+            aplicación la primera vez que la vea
+          </p>
+        </Slide>
+        <Slide>
+          <Text>Más recursos</Text>
+          <p>
+            Es más fácil encontrar tanto recursos (cursos, librerías,
+            bibliotecas) como desarrolladores que tengan los conocimientos para
+            entrar a la empresa y empezar a trabajar desde el primer dia
+          </p>
+        </Slide>
+        <Slide>
+          <Index active={2} />
+        </Slide>
+        <Slide>
+          <p>
+            Por supuesto, se puede crear toda una aplicación sin seguir ningún
+            framework conocido
+          </p>
+          <p>Puede que sea tan pequeña que no se considere necesario</p>
+          <p>
+            O simplemente no desee dedicar tiempo a seleccionar y aprender uno
+          </p>
+        </Slide>
+        <Slide>
+          <p>
+            Sin embargo, a medida que la aplicación crece, un programador
+            competente procurará seguir unas determinadas pautas que le
+            faciliten su trabajo de desarrollo y mantenimiento:
+          </p>
+        </Slide>
+        <Slide>
+          <p>Separación de presentación y lógica</p>
+          <p>Una sintaxis coherente</p>
+          <p>Generación de código repetitivo</p>
+          <p>Herramientas para agilizar el desarrollo</p>
+        </Slide>
+        <Slide>
+          <p>
+            La evolución natural será hacia que, de algún modo, se construirá su
+            propio framework
+          </p>
+        </Slide>
+        <Slide>
+          <p>
+            Hacer un desarrollo críptico y difícil de interpretar puede ser útil
+            en un concurso de código ofuscado o para presumir de “gurú”, pero{" "}
+            <strong>
+              es muy poco útil para desarrollar y mantener una aplicación.
+            </strong>
+          </p>
+        </Slide>
+        <Slide>
+          <p>
+            El coste inicial (la curva de aprendizaje) de utilizar un framework
+            se compensará probablemente en cuanto el trabajo de desarrollo
+            crezca mínimamente
+          </p>
+        </Slide>
+        <Slide>
+          <Index active={3} />
+        </Slide>
+        <Slide>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center"
+            }}
+          >
+            {this.state.logos.map((l, i) => (
+              <Image
+                key={l}
+                className="logos"
+                style={imgStyle(100, i === 0)}
+                src={l}
+              />
+            ))}
+          </div>
         </Slide>
       </Deck>
     );
--- server.js
+++ server.js
@@ -10,17 +10,21 @@ var compiler = webpack(config);
 
 var serverPort = process.env.PORT || 3000;
 
-app.use(require("webpack-dev-middleware")(compiler, {
-  publicPath: config.output.publicPath
-}));
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath
+  })
+);
 
 app.use(require("webpack-hot-middleware")(compiler));
 
-app.get("*", function(req, res) {
+app.use("/dist", express.static("dist"));
+
+app.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.listen(serverPort, "localhost", function (err) {
+app.listen(serverPort, "localhost", function(err) {
   if (err) {
     console.log(err);
     return;
